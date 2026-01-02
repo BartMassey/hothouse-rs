6689086e62befeb698a80d91fae3b03e95c6f6b7
Bart Massey <bart.massey@gmail.com>
2026-01-02 09:48:17 -0800

    added working UI example

--- examples/blinkrate.rs
+++ examples/blinkrate.rs
@@ -4,6 +4,7 @@
 use hothouse::{Hothouse, ToggleState, hal::prelude::*};
 
 use cortex_m_rt::entry;
+use num_traits::float::Float;
 use panic_halt as _;
 
 #[entry]
@@ -12,27 +13,24 @@ fn main() -> ! {
     hh.set_led(2, true).unwrap();
 
     let pots = [[4, 1], [5, 2], [6, 3]];
-    let mut rate: Option<f32> = Some(10.0);
+    let mut rate = 0.0f32;
     let mut lit = false;
 
     loop {
-        if let Some(rate) = rate {
-            let time = (1000.0 / rate) as u32;
-            hh.delay.delay_ms(time);
-            lit = !lit;
-            hh.set_led(1, lit).unwrap();
-        }
+        let time = (300.0 / 10.0.powf(rate)).floor() as u32;
+        hh.delay.delay_ms(time);
+        lit = !lit;
+        hh.set_led(1, lit).unwrap();
 
-        rate = None;
+        rate = 0.0;
         for (i, ps) in pots.iter().enumerate() {
-            let t = hh.read_toggle(i).unwrap();
+            let t = hh.read_toggle(i + 1).unwrap();
             let p = match t {
                 ToggleState::Centered => continue,
                 ToggleState::Down => ps[0],
                 ToggleState::Up => ps[1],
             };
-            let f = hh.read_knob(p).unwrap();
-            rate = Some(30.0 * f);
+            rate = hh.read_knob(p).unwrap();
             break;
         }
     }
