77781f310e3576d455cb946bdc16b4ff1783a085
Bart Massey <bart.massey@gmail.com>
2026-01-01 22:57:42 -0800

    finished initial buildout of functions

--- hothouse-pins.md
+++ hothouse-pins.md
@@ -3,31 +3,31 @@ Bart Massey 2026
 
 Taken from the [schematic](https://github.com/clevelandmusicco/HothouseExamples/wiki/Frequently-Asked-Questions#q-where-can-i-find-the-hothouse-schematics)
 
-| Hothouse  | Daisy | GPIO  | logical |
-| ---:      | ---:  |  ---: |    :--- |
-| POT1 | 23 | 16 | ADC1 |
-| POT2 | 24 | 17 | ADC2 |
-| POT3 | 25 | 18 | ADC3 |
-| POT4 | 26 | 19 | ADC4 |
-| POT5 | 27 | 20 | ADC5 |
-| POT6 | 28 | 21 | ADC6 |
-|           |       |       |         |
-| SW1\_DOWN | 11 | 10 | (SPI1\_MOSI) |
-| SW1\_UP   | 10 | 9 | (SPI1\_MISO) |
-| SW1\_DOWN | 9 | 8 | (SPI1\_SCK) |
-| SW1\_UP   | 8 | 7 | (SPI1\_NSS) |
-| SW1\_DOWN | 7 | 6 | (SDMMC1\_CK) |
-| SW1\_UP   | 6 | 5 | (SDMMC1\_CMD) |
-|           |       |       |         |
-| LED\_1 | 29 | 22 | (ADC7/DAC2) |
-| LED\_2 | 30 | 23 | (ADC8/DAC1) |
-|           |       |       |         |
-| FSW\_1 | 32 | 25 | (ADC10) |
-| FSW\_2 | 33 | 26 | (SAI2\_SD\_A) |
+| Hothouse  | Daisy | crate | MCU  | function      |
+| ---:      | ---:  | ---:  | ---: | :---          |
+| POT1      | 23    | 16    | PA3  | ADC1          |
+| POT2      | 24    | 17    | PB1  | ADC2          |
+| POT3      | 25    | 18    | PA7  | ADC3          |
+| POT4      | 26    | 19    | PA6  | ADC4          |
+| POT5      | 27    | 20    | PC1  | ADC5          |
+| POT6      | 28    | 21    | PC4  | ADC6          |
+|           |       |       |      |               |
+| SW1\_DOWN | 11    | 10    | PB5  | (SPI1\_MOSI)  |
+| SW1\_UP   | 10    | 9     | PB4  | (SPI1\_MISO)  |
+| SW2\_DOWN | 9     | 8     | PG11 | (SPI1\_SCK)   |
+| SW2\_UP   | 8     | 7     | PG10 | (SPI1\_NSS)   |
+| SW3\_DOWN | 7     | 6     | PC12 | (SDMMC1\_CK)  |
+| SW3\_UP   | 6     | 5     | PD2  | (SDMMC1\_CMD) |
+|           |       |       |      |               |
+| LED\_1    | 29    | 22    | PA5  | (ADC7/DAC2)   |
+| LED\_2    | 30    | 23    | PA4  | (ADC8/DAC1)   |
+|           |       |       |      |               |
+| FSW\_1    | 32    | 25    | PA0  | (ADC10)       |
+| FSW\_2    | 33    | 26    | PD11 | (SAI2\_SD\_A) |
 
-| Hothouse | Daisy |
-|    ---: |   ---: |
-| AUDIO\_IN\_L  | 16 |
-| AUDIO\_IN\_R  | 17 |
-| AUDIO\_OUT\_L | 18 |
-| AUDIO\_OUT\_R | 19 |
+| Hothouse      | Daisy  |
+|          ---: |   ---: |
+| AUDIO\_IN\_L  | 16     |
+| AUDIO\_IN\_R  | 17     |
+| AUDIO\_OUT\_L | 18     |
+| AUDIO\_OUT\_R | 19     |
--- src/lib.rs
+++ src/lib.rs
@@ -2,30 +2,62 @@
 #![no_std]
 
 extern crate cortex_m;
+pub use daisy as board;
+pub use daisy::hal;
+pub use daisy::pac;
 use daisy::{
-    self,
     hal::{prelude::*, adc, delay, gpio},
-    pac,
     Board,
 };
 
 pub enum HothouseError {
     BadIndex,
+    SwitchFailure,
 }
 
-struct Knobs {
-    knob1: gpio::gpioa::PA3<gpio::Analog>,
-    knob2: gpio::gpiob::PB1<gpio::Analog>,
-    knob3: gpio::gpioa::PA7<gpio::Analog>,
-    knob4: gpio::gpioa::PA6<gpio::Analog>,
-    knob5: gpio::gpioc::PC1<gpio::Analog>,
-    knob6: gpio::gpioc::PC4<gpio::Analog>,
+pub struct Knobs {
+    pub knob1: gpio::gpioa::PA3<gpio::Analog>,
+    pub knob2: gpio::gpiob::PB1<gpio::Analog>,
+    pub knob3: gpio::gpioa::PA7<gpio::Analog>,
+    pub knob4: gpio::gpioa::PA6<gpio::Analog>,
+    pub knob5: gpio::gpioc::PC1<gpio::Analog>,
+    pub knob6: gpio::gpioc::PC4<gpio::Analog>,
+}
+
+pub struct Spdt<Down, Up> {
+    pub down: Down,
+    pub up: Up,
+}
+
+pub struct Toggles {
+    pub sw1: Spdt<gpio::gpiob::PB5<gpio::Input>, gpio::gpiob::PB4<gpio::Input>>,
+    pub sw2: Spdt<gpio::gpiog::PG11<gpio::Input>, gpio::gpiog::PG10<gpio::Input>>,
+    pub sw3: Spdt<gpio::gpioc::PC12<gpio::Input>, gpio::gpiod::PD2<gpio::Input>>,
+}
+
+pub enum ToggleState {
+    Down,
+    Centered,
+    Up,
+}
+
+pub struct Leds {
+    pub led1: gpio::gpioa::PA5<gpio::Output>,
+    pub led2: gpio::gpioa::PA4<gpio::Output>,
+}
+
+pub struct Footswitches {
+    pub fsw1: gpio::gpioa::PA0<gpio::Input>,
+    pub fsw2: gpio::gpiod::PD11<gpio::Input>,
 }
 
 pub struct Hothouse {
-    board: Board,
+    pub board: Board,
     knob_adc: adc::Adc<pac::ADC1, adc::Enabled>,
-    knobs: Knobs,
+    pub knobs: Knobs,
+    pub leds: Leds,
+    pub toggles: Toggles,
+    pub footswitches: Footswitches,
 }
 
 impl Hothouse {
@@ -46,6 +78,31 @@ impl Hothouse {
             knob6: pins.GPIO.PIN_21.into_analog(),
         };
 
+        let toggles = Toggles {
+            sw1: Spdt {
+                down: pins.GPIO.PIN_10.into_pull_up_input(),
+                up: pins.GPIO.PIN_9.into_pull_up_input(),
+            },
+            sw2: Spdt {
+                down: pins.GPIO.PIN_8.into_pull_up_input(),
+                up: pins.GPIO.PIN_7.into_pull_up_input(),
+            },
+            sw3: Spdt {
+                down: pins.GPIO.PIN_6.into_pull_up_input(),
+                up: pins.GPIO.PIN_5.into_pull_up_input(),
+            },
+        };
+
+        let leds = Leds {
+            led1: pins.GPIO.PIN_22.into_push_pull_output(),
+            led2: pins.GPIO.PIN_23.into_push_pull_output(),
+        };
+
+        let footswitches = Footswitches {
+            fsw1: pins.GPIO.PIN_25.into_pull_up_input(),
+            fsw2: pins.GPIO.PIN_26.into_pull_up_input(),
+        };
+
         // Configure ADC.
         let mut delay = delay::Delay::new(cp.SYST, ccdr.clocks);
         let mut knob_adc = adc::Adc::adc1(
@@ -55,10 +112,10 @@ impl Hothouse {
             ccdr.peripheral.ADC12,
             &ccdr.clocks,
         )
-        .enable();
+            .enable();
         knob_adc.set_resolution(adc::Resolution::SixteenBit);
 
-        Self { board, knob_adc, knobs }
+        Self { board, knob_adc, knobs, toggles, leds, footswitches }
     }
 
     pub fn read_knob(&mut self, knob_id: usize) -> Result<f32, HothouseError> {
@@ -73,4 +130,39 @@ impl Hothouse {
         }.unwrap();
         Ok(position as f32 / 65_635.0)
     }
+
+    pub fn read_toggle(&mut self, toggle_id: usize) -> Result<ToggleState, HothouseError> {
+        let sets = match toggle_id {
+            1 => (self.toggles.sw1.down.is_high(), self.toggles.sw1.up.is_high()),
+            2 => (self.toggles.sw2.down.is_high(), self.toggles.sw2.up.is_high()),
+            3 => (self.toggles.sw3.down.is_high(), self.toggles.sw3.up.is_high()),
+            _ => return Err(HothouseError::BadIndex),
+        };
+        match sets {
+            (false, true) => Ok(ToggleState::Down),
+            (true, true) => Ok(ToggleState::Centered),
+            (true, false) => Ok(ToggleState::Up),
+            (false, false) => Err(HothouseError::SwitchFailure),
+        }
+    }
+
+    pub fn set_led(&mut self, led_id: usize, state: bool) -> Result<(), HothouseError> {
+        match (led_id, state) {
+            (1, true) => self.leds.led1.set_high(),
+            (2, true) => self.leds.led2.set_high(),
+            (1, false) => self.leds.led1.set_low(),
+            (2, false) => self.leds.led2.set_low(),
+            _ => return Err(HothouseError::BadIndex),
+        }
+        Ok(())
+    }
+
+    pub fn read_footswitch(&mut self, fsw_id: usize) -> Result<bool, HothouseError> {
+        match fsw_id {
+            1 => Ok(self.footswitches.fsw1.is_low()),
+            2 => Ok(self.footswitches.fsw2.is_low()),
+            _ => Err(HothouseError::BadIndex),
+        }
+    }
 }
+
